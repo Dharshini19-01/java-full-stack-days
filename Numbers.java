8cdaa9b0c614079b9358944a314b939125721544
priyadharshini <priyadharshini.b19012006@gamil.com>
2025-12-11 09:43:29 +0530

    resources  added for day1

---
+++ Numbers.java
@@ -0,0 +1,17 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int p=12;
+        int q=15;
+        int r=18;
+        if (p>q && p>r){
+            System.out.println(p+ " is Greater");
+        }
+        else if(q>r && q>p){
+            System.out.println(q+ "is Greater");
+        }
+        else{
+            System.out.println(r+ " is Greater");
+        }
+        
+    }
+}
