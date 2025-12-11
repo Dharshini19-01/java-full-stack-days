4241813d82634b582b8980354c128ecca92ebfb7
priyadharshini <priyadharshini.b19012006@gamil.com>
2025-12-11 10:23:43 +0530

    added ignore files

--- Leap.java
+++ Leap.java
@@ -1,5 +1,9 @@
 public class Leap {
     public static void main(String[] args) {
+        int year=2004;
+        if(year%4==0 && year%400==0||year%100!=0){
+            System.out.println(year+"is leap year");
+        }
         
     }
 }
--- Two.java
+++ Two.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Two {
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         if (a<b){
